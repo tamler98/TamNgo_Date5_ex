ea55006f3c3591bd8268bb4f1d0f0198c8978927
tamler98 <120762094+tamler98@users.noreply.github.com>
2023-01-06 14:59:24 +0700

    Update OrderDetailRepository.java

--- src/main/java/repository/OrderDetailRepository.java
+++ src/main/java/repository/OrderDetailRepository.java
@@ -8,9 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface OrderDetailRepository extends CrudRepository<OrderDetails, Integer> {
-    @Query(value = "select * from orderdetails as od\n" +
-            "inner join orders as o\n" +
-            "on od.orderId = o.id\n" +
-            "where o.id = ?1", nativeQuery = true)
+    @Query(value = "select * from orderdetails where orderId = ?1", nativeQuery = true)
     List<OrderDetails> getOrderDetailByOrderId(int id);
 }
